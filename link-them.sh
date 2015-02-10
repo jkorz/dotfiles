fc3f2c5df793ca3b4a5a8e8fc556ad8aa4203a10
Joe Korzeniewski <joe.korzeniewski@phishme.com>
2015-02-10 13:51:41 -0500

    Make linking script work

--- link-them.sh
+++ link-them.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
 CWD=`pwd`
-BAK=$HOME/.dotfiles-backup/
 
+CONFIGS=`ls -F config | grep '[a-zA-Z0-9]$'`
+BINS=`ls -F bin | grep '\*$' | sed 's/\*$//'`
+
+BAK=$HOME/.dotfiles-backup
 mkdir -p $BAK
 
-mv $HOME/.vimrc $BAK
-ln -s $CWD/config/vimrc $HOME/.vimrc
+for i in $CONFIGS;
+do
+    if [ -f "$HOME/.$i" ]; then cp $HOME/.$i $BAK/$i.bak; fi
+    rm $HOME/.$i
+    ln -s $CWD/config/$i $HOME/.$i
+done
+
+BAK=$HOME/.dotfiles-backup/bin
+mkdir -p $BAK
+
+for i in $BINS;
+do
+    if [ -f "$HOME/bin/.$i" ]; then cp $HOME/bin/$i $BAK/$i.bak; fi
+    rm $HOME/bin/$i
+    ln -s $CWD/bin/$i $HOME/bin/$i
+done
+
