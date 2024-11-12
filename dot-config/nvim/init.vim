76437fa01326c4a0fb9904b57e534884f4ec5ab9
Joe Korzeniewski <665255+jkorz@users.noreply.github.com>
2024-11-12 13:01:03 -0500

    Make linter work

--- dot-config/nvim/init.vim
+++ dot-config/nvim/init.vim
@@ -23,6 +23,11 @@ nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
 nnoremap <leader>fs <cmd>Telescope search_history<cr>
+nnoremap <leader>af <cmd>lua vim.lsp.buf.format()<cr>
+nnoremap <leader>. :call OpenTestAlternate()<cr>
+inoremap <c-/> <Plug>(copilot-suggest)
+nnoremap <leader>m <cmd>MCstart<cr>
+vnoremap <leader>m <cmd>MCstart<cr>
 
 " Move around splits with <c-hjkl>
 nnoremap <C-j> <C-w>j
@@ -57,5 +62,38 @@ autocmd InsertLeave * match ExtraWhitespace /\s\+$/
 autocmd BufWinLeave * call clearmatches()
 autocmd BufRead,BufNewFile *.erb set filetype=eruby.html
 
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" SWITCH BETWEEN TEST AND PRODUCTION CODE
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+function! OpenTestAlternate()
+  let new_file = AlternateForCurrentFile()
+  exec ':e ' . new_file
+endfunction
+function! AlternateForCurrentFile()
+  let current_file = expand("%")
+  let new_file = current_file
+  let in_spec = match(current_file, '^spec/') != -1
+  let going_to_spec = !in_spec
+  let in_app = match(current_file, '\<controllers\>') != -1 || match(current_file, '\<models\>') != -1 || match(current_file, '\<views\>') != -1 || match(current_file, '\<decorators\>') != -1 || match(current_file, '\<services\>') != -1 || match(current_file, '\<helpers\>') != -1 || match(current_file, '\<policies\>') != -1
+  if going_to_spec
+    if in_app
+      let new_file = substitute(new_file, '^app/', '', '')
+    end
+    let new_file = substitute(new_file, '\.rb$', '_spec.rb', '')
+    let new_file = 'spec/' . new_file
+  else
+    let new_file = substitute(new_file, '_spec\.rb$', '.rb', '')
+    let new_file = substitute(new_file, '^spec/', '', '')
+    if in_app
+      let new_file = 'app/' . new_file
+    end
+  endif
+  return new_file
+endfunction
+
 lua require('plugins')
 lua require('lspconfig').ruby_lsp.setup{}
+
+"lua vim.g.autoformat=false
+lua vim.cmd [[autocmd! BufWritePre *]]
+"autocmd FileType *.rb,*.erb setlocal formatoptions-=cro
--- dot-config/nvim/lua/plugins.lua
+++ dot-config/nvim/lua/plugins.lua
@@ -1,60 +1,71 @@
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use('dmmulroy/tsc.nvim')
-    use 'williamboman/mason.nvim'
-    use 'williamboman/mason-lspconfig.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'flazz/vim-colorschemes'
-    use 'bling/vim-airline'
-    use 'airblade/vim-gitgutter'
-    use 'mileszs/ack.vim'
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-fugitive'
-    use 'rbong/vim-flog'
-    use 'jgdavey/vim-blockle'
-    use 'tpope/vim-surround'
-    use 'tpope/vim-unimpaired'
-    use 'tpope/vim-repeat'
-    use 'tpope/vim-eunuch'
-    use 'github/copilot.vim'
-    use 'prettier/vim-prettier'
-    use 'yuezk/vim-js'
-    use 'maxmellon/vim-jsx-pretty'
-    use {
-	    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	    -- or                            , branch = '0.1.x',
-	    requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use 'nvim-tree/nvim-web-devicons'
-    use {
-      "nvim-telescope/telescope-frecency.nvim",
-      config = function()
-        require("telescope").load_extension "frecency"
-      end,
-    }
-    use({
+  use('dmmulroy/tsc.nvim')
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'flazz/vim-colorschemes'
+  use 'bling/vim-airline'
+  use 'airblade/vim-gitgutter'
+  use 'mileszs/ack.vim'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-fugitive'
+  use 'rbong/vim-flog'
+  use 'jgdavey/vim-blockle'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-unimpaired'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-eunuch'
+  use 'github/copilot.vim'
+  use 'prettier/vim-prettier'
+  use 'yuezk/vim-js'
+  use 'maxmellon/vim-jsx-pretty'
+  use { 'CopilotC-Nvim/CopilotChat.nvim', branch = 'canary' }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+    "nvim-telescope/telescope-frecency.nvim",
+    config = function()
+      require("telescope").load_extension "frecency"
+    end,
+  }
+  use {
     'ray-x/navigator.lua',
     requires = {
-        { 'ray-x/guihua.lua', run = 'cd lua/fzy && make' },
-        { 'neovim/nvim-lspconfig' },
-    },
-})
+      { 'ray-x/guihua.lua', run = 'cd lua/fzy && make' },
+      { 'neovim/nvim-lspconfig' },
+    }
+  }
+  use {
+    "smoka7/multicursors.nvim",
+    requires = { 'nvimtools/hydra.nvim' }
+  }
 
 end)
 require('tsc').setup {
-    -- enable = true,
-    -- enable_ts = true,
-    -- enable_ts_imports = true,
-    -- enable_formatting = true,
+  -- enable = true,
+  -- enable_ts = true,
+  -- enable_ts_imports = true,
+  -- enable_formatting = true,
 }
 require'navigator'.setup()
+-- require("CopilotChat").setup()
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = { "rubocop" },
+  ensure_installed = { "rubocop", "ruby_lsp" },
+  on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = true
+  end,
 }
+-- require('multicursors').start()
 
 return
